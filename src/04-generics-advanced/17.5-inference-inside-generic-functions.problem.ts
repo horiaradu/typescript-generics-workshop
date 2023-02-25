c12daf8df0ea433fb90802c7613ca72f11cd7ddc
Horia Radu <horia.radu@e-spres-oh.com>
2023-02-25 21:48:55 +0200

    wip

--- src/04-generics-advanced/17.5-inference-inside-generic-functions.problem.ts
+++ src/04-generics-advanced/17.5-inference-inside-generic-functions.problem.ts
@@ -8,8 +8,8 @@ export function remapPerson<Key extends keyof Person>(
   key: Key,
   value: Person[Key],
 ): Person[Key] {
-  if (key === "birthdate") {
-    return new Date();
+  if (key === 'birthdate') {
+    return new Date() as Person[Key];
   }
 
   return value;
